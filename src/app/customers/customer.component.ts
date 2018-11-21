af6d54ba9a870780fc5f9ea16a9c55dc5146c3cb
Branham <jonathonbranham@nexsystech.com>
2018-11-21 12:20:52 -0500

    complete with errors

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -67,6 +67,10 @@ export class CustomerComponent implements OnInit {
     );
   }
 
+  addAddress(): void {
+    this.addresses.push(this.buildAddress());
+  }
+
   buildAddress(): FormGroup {
     return this.fb.group({
         addressType:'home',
